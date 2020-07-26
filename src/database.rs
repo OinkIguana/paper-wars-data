1b5a4f604e8935924b9275ef2ad1404491e508d7
Cameron Eldridge <cameldridge@gmail.com>
2020-07-26 14:00:17 -0400

    Permissions for the database

--- src/database.rs
+++ src/database.rs
@@ -1,11 +1,14 @@
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+use std::sync::Arc;
 
 pub type DbConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
-/// Handles connections to the database.
+/// Handles connections to the database. Clones of this handle share the same
+/// underlying connection pool.
+#[derive(Clone)]
 pub struct Database {
-    pool: Pool<ConnectionManager<PgConnection>>,
+    pool: Arc<Pool<ConnectionManager<PgConnection>>>,
 }
 
 impl Database {
@@ -13,7 +16,7 @@ impl Database {
     pub fn connect(url: impl Into<String>) -> anyhow::Result<Self> {
         let manager = ConnectionManager::new(url);
         Ok(Database {
-            pool: Pool::new(manager)?,
+            pool: Arc::new(Pool::new(manager)?),
         })
     }
 
