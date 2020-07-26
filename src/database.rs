cb81429bd5eb5b3d3cbda57abb76dbc5a3c1e19a
Cameron Eldridge <cameldridge@gmail.com>
2020-07-25 22:45:49 -0400

    Add some more things for database

--- src/database.rs
+++ src/database.rs
@@ -1,19 +1,35 @@
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 
-type PgConnectionManager = ConnectionManager<PgConnection>;
+pub type Conn = PooledConnection<ConnectionManager<PgConnection>>;
 
+/// Handles connections to the database.
 pub struct Database {
     pool: Pool<ConnectionManager<PgConnection>>,
 }
 
 impl Database {
-    pub fn connect(url: impl Into<String>) -> Result<Self, Box<dyn std::error::Error>> {
+    /// Creates a `Database` connected to the provided URL.
+    pub fn connect(url: impl Into<String>) -> anyhow::Result<Self> {
         let manager = ConnectionManager::new(url);
-        Ok(Database { pool: Pool::new(manager)? })
+        Ok(Database {
+            pool: Pool::new(manager)?,
+        })
     }
 
-    pub fn connection(&self) -> Result<PooledConnection<PgConnectionManager>, Box<dyn std::error::Error>> {
+    /// Gets a connection to this database.
+    pub fn connection(&self) -> anyhow::Result<Conn> {
         self.pool.get().map_err(Into::into)
+    }
+
+    /// Starts a transaction using a connection to this database. The provided function
+    /// will be called with that connection.
+    pub fn transaction<T, E, F>(&self, proc: F) -> anyhow::Result<T>
+    where
+        E: 'static + Sync + Send + std::error::Error,
+        F: Fn(&Conn) -> Result<T, E>,
+    {
+        let conn = self.connection()?;
+        conn.transaction(|| proc(&conn).map_err(Into::into))
     }
 }
