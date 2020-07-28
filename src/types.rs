d594b93858e01899bcf5f2d16df53903d0109611
Cameron Eldridge <cameldridge@gmail.com>
2020-07-27 23:29:01 -0400

    Implement graphql traits for the types

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,7 @@
 use diesel_derive_enum::DbEnum;
+use juniper::GraphQLEnum;
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug, DbEnum)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug, DbEnum, GraphQLEnum)]
 #[DieselType = "Contributor_role"]
 #[PgType = "contributor_role"]
 pub enum ContributorRole {
